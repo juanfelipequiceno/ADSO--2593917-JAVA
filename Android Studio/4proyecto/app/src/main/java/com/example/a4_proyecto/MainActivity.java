576fb297fbdb03fdf7bce2e22d6712902a037f47
juanfelipequiceno <felipequiceno78@gmail.com>
2024-02-19 16:47:35 -0500

    cuarto proyecto

---
+++ Android Studio/4proyecto/app/src/main/java/com/example/a4_proyecto/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.a4_proyecto;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    Button insertar = findViewById(R.id.btnInsertar);
+
+
+}
---
+++ Android Studio/4proyecto/app/src/main/java/com/example/a4_proyecto/vistaInsertar.java
@@ -0,0 +1,14 @@
+package com.example.a4_proyecto;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class vistaInsertar extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vista_insertar);
+    }
+}
